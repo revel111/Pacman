c84bb5ea53efbb4b6a2a022dc79f49a0e291457d
revel111 <roflankaa@gmail.com>
2023-05-16 12:21:12 +0200

    creating map

--- src/windows/Game.java
+++ src/windows/Game.java
@@ -103,15 +103,15 @@ public class Game extends JFrame implements KeyListener {
             }
         }).start();
 
-        new Thread(() -> {
-            while (tableModel.isInGame())
-                tableModel.getPacman().movePac();
-            try {
-                end();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+//        new Thread(() -> {
+//            while (tableModel.isInGame())
+//                tableModel.getPacman().movePac();
+//            try {
+//                end();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }).start();
 
 //        new Thread(() -> {
 //            while (tableModel.isInGame())
@@ -131,29 +131,30 @@ public class Game extends JFrame implements KeyListener {
 //            }
 //        }).start();
 //
-        for (int i = 0; i < tableModel.getGhosts().size(); i++) {
-            int finalI = i;
-            new Thread(() -> {
-                while (tableModel.isInGame())
-                    tableModel.getGhosts().get(finalI).moveGhost();
-            }).start();
-        }
 
-        for (int i = 0; i < tableModel.getGhosts().size(); i++) {
-            int finalI = i;
-            new Thread(() -> {
-                while (tableModel.isInGame()) {
-                    Random random = new Random();
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    int boost = random.nextInt(5);
-                    tableModel.getGhosts().get(finalI).setBoost(boost);
-                }
-            }).start();
-        }
+//        for (int i = 0; i < tableModel.getGhosts().size(); i++) {
+//            int finalI = i;
+//            new Thread(() -> {
+//                while (tableModel.isInGame())
+//                    tableModel.getGhosts().get(finalI).moveGhost();
+//            }).start();
+//        }
+//
+//        for (int i = 0; i < tableModel.getGhosts().size(); i++) {
+//            int finalI = i;
+//            new Thread(() -> {
+//                while (tableModel.isInGame()) {
+//                    Random random = new Random();
+//                    try {
+//                        Thread.sleep(5000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    int boost = random.nextInt(5);
+//                    tableModel.getGhosts().get(finalI).setBoost(boost);
+//                }
+//            }).start();
+//        }
 
         jTable.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {  //draw map
             @Override
